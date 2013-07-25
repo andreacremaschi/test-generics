9b37a7f6bc28ed43cbc2d47a8228f02eb759cc62
Andrea Cremaschi <andreacremaschi@libero.it>
2013-07-25 10:44:18 +0200

    Initial Commit

---
+++ test_generics/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  test_generics
+//
+//  Created by Andrea Cremaschi on 25/07/13.
+//  Copyright (c) 2013 midapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
